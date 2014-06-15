6662fba4eaf202f268e6e80b5358c1f1aea1780e
Artyom Philonenko <greshnikk@gmail.com>
2014-06-15 23:02:26 +0400

    Added debug toggle

--- src/translator/Main.java
+++ src/translator/Main.java
@@ -1,11 +1,15 @@
 package translator;
 
 public class Main {
+	private static final boolean debug = true;
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		try {
 			LexicalAnalysis test = new LexicalAnalysis();
-			test.convert("2X1^2 - ( 3+ 2)X2^2 -333.1415X1X2e^2X1sin(X1) + 0.17E-27*PI*lg(10)+ 12E13");
+			SemanticAnalysis test2 = new SemanticAnalysis();
+			String a = test
+					.convert("2X1^2 - ( 3+ 2)X2^2 -333.1415X1X2e^2X1sin(X1) + 0.17E-27*PI*lg(10)+ 12E13");
+			String b = test2.convert(a);
 		} catch (NumberFormatException e) {
 			Main.log("Wrong number format. " + e.getMessage());
 		} catch (Exception e) {
@@ -15,7 +19,8 @@ public class Main {
 	}
 
 	public static void log(Object input) {
-		System.out.println(input);
+		if (debug)
+			System.out.println(input);
 	}
 
 }
--- src/translator/SemanticAnalysis.java
+++ src/translator/SemanticAnalysis.java
@@ -6,5 +6,10 @@ package translator;
  * @since 2014-05-24
  */
 public class SemanticAnalysis {
-
+	public String convert (String input) {
+		String output = null;
+		
+		Main.log(output);
+		return output;
+	}
 }
