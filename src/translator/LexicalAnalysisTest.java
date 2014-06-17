60ca7276323afa236fd15b6fba42522f093f4236
Artyom Philonenko <greshnikk@gmail.com>
2014-06-17 22:17:33 +0400

    Added Semantic Analysis unit test.

--- src/translator/LexicalAnalysisTest.java
+++ src/translator/LexicalAnalysisTest.java
@@ -12,8 +12,6 @@ public class LexicalAnalysisTest {
 		assertTrue(tester
 				.convert(
 						"2X1^2 - ( 3+ 2)X2^2 -333.1415X1X2e^2X1sin(X1) + 0.17E-27*PI*lg(10)+ 12E13")
-				.equals("N0 O3 I0 O5 N1 S4 O2 S4 S1 S4 N2 O1 S4 N3 S2 I1 O5 N4 S4 O2 N5 O3 I0 O3 I1 O3 C2 O5 N6 O3 I0 O3 F13 S1 I0 S2 S4 O1 S4 N7 O3 C1 O3 F5 S1 N8 S2 O1 S4 N9 "));
+				.equals("N0 O3 I0 O5 N1 O2 S1 N2 O1 N3 S2 I1 O5 N4 O2 N5 O3 I0 O3 I1 O3 C2 O5 N6 O3 I0 O3 F13 S1 I0 S2 O1 N7 O3 C1 O3 F5 S1 N8 S2 O1 N9 "));
 	}
 }
-//"2 X1 2 ^ * 3 2 + X2 2 ^ * - -333.1415 X1 * X2 * e 2 ^ * X1 * sin X1 1F * + 0.17E-27 PI * lg 10 1F * + 12E13 +"
-//"N0 I0 N1 O5 O3 N2"
--- src/translator/SemanticAnalysisTest.java
+++ src/translator/SemanticAnalysisTest.java
@@ -11,8 +11,9 @@ public class SemanticAnalysisTest {
 		SemanticAnalysis tester = new SemanticAnalysis();
 		assertTrue(tester
 				.convert(
-						"N0 O3 I0 O5 N1 S4 O2 S4 S1 S4 N2 O1 S4 N3 S2 I1 O5 N4 S4 O2 N5 O3 I0 O3 I1 O3 C2 O5 N6 O3 I0 O3 F13 S1 I0 S2 S4 O1 S4 N7 O3 C1 O3 F5 S1 N8 S2 O1 S4 N9 ")
-				.equals("2 X1 2 ^ * 3 2 + X2 2 ^ * - -333.1415 X1 * X2 * e 2 ^ * X1 * sin X1 1F * + 0.17E-27 PI * lg 10 1F * + 12E13 +"));
+						"N0 O3 I0 O5 N1 O2 S1 N2 O1 N3 S2 I1 O5 N4 O2 N5 O3 I0 O3 I1 O3 C2 O5 N6 O3 I0 O3 F13 S1 I0 S2 O1 N7 O3 C1 O3 F5 S1 N8 S2 O1 N9 ")
+				.equals("N0 I0 N1 O5 O3 N2 N3 O1 I1 N4 O5 O3 O2 N5 I0 O3 I1 O3 C2 N6 O5 O3 I0 O3 F13 I0 !1F! O3 O1 N7 C1 O3 F5 N8 !1F! O3 O1 N9 O1"));
 	}
-
 }
+//"2 X1 2 ^ * 3 2 + X2 2 ^ * - -333.1415 X1 * X2 * e 2 ^ * X1 * sin X1 1F * + 0.17E-27 PI * lg 10 1F * + 12E13 +"
+//"N0 I0 N1 O5 O3 N2 N3 O1 I1 N4 O5 O3 O2 N5 I0 O3 I1 O3 C2 N6 O5 O3 I0 O3 F13 I0 !1F! O3 O1 N7 C1 O3 F5 N8 !1F! O3 O1 N9 O1"
